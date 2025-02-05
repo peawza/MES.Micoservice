249c9227f968387f160cb20e26579e3d820671a1
Supachai <supachai@CSIGROUPS.COM>
2025-02-05 11:46:39 +0700

    update DB

---
+++ Service/Inventory/InventoryMasterPostgreSQLDB/ProductionDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public class SystemDbContext : DbContext
+    {
+
+
+        public SystemDbContext(DbContextOptions<SystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        public static void InitialService(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<SystemDbContext>(options =>
+                options.UseNpgsql(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("public");
+
+
+        }
+    }
+}
