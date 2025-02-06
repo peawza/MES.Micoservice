322cb967cbf4c973b3f44660d775275fea56a329
Supachai <supachai@CSIGROUPS.COM>
2025-02-06 15:04:09 +0700

    update config ocelot

--- Gateway/GatewaySolution/Program.cs
+++ Gateway/GatewaySolution/Program.cs
@@ -5,19 +5,21 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddAppAuthetication();
-if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
-{
-    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
-}
-else
-{
-    builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-}
+//if (builder.Environment.EnvironmentName.ToString().ToLower().Equals("production"))
+//{
+//    builder.Configuration.AddJsonFile("ocelot.Production.json", optional: false, reloadOnChange: true);
+//}
+//else
+//{
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+//}
 builder.Services.AddOcelot(builder.Configuration);
 
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 //app.MapGet("/", () => "Hello World!");
 app.UseOcelot().GetAwaiter().GetResult();
 await app.RunAsync();
