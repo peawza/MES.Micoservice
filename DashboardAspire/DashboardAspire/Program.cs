02f53a12778358cc2972d14241f7ee99bc5050fb
Supachai <supachai@CSIGROUPS.COM>
2025-02-17 14:01:33 +0700

    update Code pms081

--- DashboardAspire/DashboardAspire/Program.cs
+++ DashboardAspire/DashboardAspire/Program.cs
@@ -1,10 +1,33 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
 
 var apiServiceEmail = builder.AddProject<Projects.Service_Email>("ServiceEmail");
 var apiServiceCronjob = builder.AddProject<Projects.Service_CronJob>("ServiceCronJob");
+
+
+// เพิ่ม Services ใน Aspire
+var apiAuth = builder.AddProject<Projects.Authentication>("ServiceAuth");
+var apiInventoryMaster = builder.AddProject<Projects.InventoryMasterAPI>("ServiceInventoryMaster");
+var apiInventoryOperation = builder.AddProject<Projects.InventoryOperationAPI>("ServiceInventoryOperation");
+var apiInventoryReport = builder.AddProject<Projects.InventoryReportAPI>("ServiceInventoryReport");
+
+//*note Rename
+/*
+var apiMaintenanceMaster = builder.AddProject<Projects.ProductionMasterAPI>("ServiceMaintenanceMaster");
+var apiMaintenanceOperation = builder.AddProject<Projects.ProductionOperationAPI>("ServiceMaintenanceOperation");
+var apiMaintenanceReport = builder.AddProject<Projects.ProductionReportAPI>("ServiceMaintenanceReport");
+*/
+//*note Rename
+
+var apiProductionMaster = builder.AddProject<Projects.ProductionMasterAPI>("ServiceProductionMaster");
+var apiProductionOperation = builder.AddProject<Projects.ProductionOperationAPI>("ServiceProductionOperation");
+var apiProductionReport = builder.AddProject<Projects.ProductionReportAPI>("ServiceProductionReport");
+var apiQualityMaster = builder.AddProject<Projects.QualityMasterAPI>("ServiceQualityMaster");
+var apiQualityOperation = builder.AddProject<Projects.QualityOperationAPI>("ServiceQualityOperation");
+var apiQualityReport = builder.AddProject<Projects.QualityReportAPI>("ServiceQualityReport");
+
 
 
 builder.AddProject<Projects.WEB_APP>("webfrontend")
---
+++ Gateway/GatewayYarpSolution/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.RequireAuthenticatedSignIn = true; // บังคับให้ต้อง Sign-in
+})
+.AddJwtBearer(cfg =>
+{
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["JwtIssuer"],
+        ValidAudience = builder.Configuration["JwtIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtKey"])),
+
+        ClockSkew = TimeSpan.Zero // ลบ delay ของ token เมื่อหมดอายุ
+    };
+});
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("BearerAuth", policy =>
+        policy.RequireAuthenticatedUser());
+});
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapReverseProxy().RequireAuthorization("BearerAuth");
+
+app.Run();
